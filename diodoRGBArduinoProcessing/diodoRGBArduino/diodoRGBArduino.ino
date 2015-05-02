bd61eab6603fe9d18d9a7ba8c5dfeec1854a05e5
Antonio Gómez <agomezgar@gmail.com>
2015-05-02 12:43:41 +0200

    Tutorial colores entre Arduino y Processing
    Tutorial Colores en diodo RGB.

---
+++ diodoRGBArduinoProcessing/diodoRGBArduino/diodoRGBArduino.ino
@@ -0,0 +1,21 @@
+int rojo=9;
+int verde=10;
+int azul=11;
+int lRojo,lVerde,lAzul,eRojo,eVerde,eAzul;
+void setup(){
+  
+}
+
+void loop(){
+  lRojo=analogRead(A0);
+  lVerde=analogRead(A1);
+  lAzul=analogRead(A2);
+  eRojo=map(lRojo,0,1023,0,255);
+  eVerde=map(lVerde,0,1023,0,255);
+  eAzul=map(lAzul,0,1023,0,255);
+ analogWrite(rojo,eRojo); 
+ analogWrite(azul,eAzul);
+ analogWrite(verde,eVerde);
+ delay(100);
+  
+}
