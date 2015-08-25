4c78cd67537443a2011aa38d240eab1065e2cc0d
Antonio Gómez <agomezgar@gmail.com>
2015-08-25 13:31:09 +0200

    Create codigoBuscaSorteos.py

---
+++ codigoBuscaSorteos.py
@@ -0,0 +1,58 @@
+import tweepy
+import serial
+import time
+ 
+# Consumer keys and access tokens, used for OAuth
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+ 
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+ 
+# Creation of the actual interface, using authentication
+api = tweepy.API(auth)
+
+
+# Sample method, used to update a status
+#api.update_status(status='En fin... La noche es joven...')
+print ("Preparando bÃºsqueda:")
+
+
+busqueda=tweepy.Cursor(api.search, q='sorteo RT',lang='es').items()
+#El string presentacion me permite avisar a mis followers que inicio al bot.
+presentacion='Atentos followers que inicio experimento con bot buscasorteos. Don\'t be afraid. Con fecha: '+time.strftime("%I:%M:%S")
+print (presentacion)
+#api.update_status(status=presentacion)
+while True:
+   
+      
+      
+      try:
+           
+            tuit=busqueda.next()          
+            print ("Texto: ")
+            frase= (tuit.text.encode('utf-8'))
+            print (frase)
+            
+            if (not("b'RT" in str(frase))):
+                  print ("Tweet original")
+                  api.retweet(tuit.id)
+                  api.create_friendship(tuit.author.id)
+                  time.sleep(10)
+               
+      except tweepy.TweepError:
+            print ("Error de Tweepy")
+            time.sleep(60*15)
+            busqueda=tweepy.Cursor(api.search, q='#Arduino',lang='es').items()
+            continue
+          
+      except StopIteration:
+            busqueda=tweepy.Cursor(api.search, q='#Arduino',lang='es').items()
+            print ("Busqueda terminada. Descansando...")
+            time.sleep(60)
+            continue
+
+
