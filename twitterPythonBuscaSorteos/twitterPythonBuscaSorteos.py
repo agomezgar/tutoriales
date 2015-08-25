827702626f89aabebeb0d48fde5606d4e885c1ca
Antonio Gómez <agomezgar@gmail.com>
2015-08-25 13:15:28 +0200

    Código en Python para buscar sorteos en Twitter

--- twitterPythonBuscaSorteos/twitterPythonBuscaSorteos.py
+++ twitterPythonBuscaSorteos/twitterPythonBuscaSorteos.py
@@ -1,4 +1,3 @@
-
 import tweepy
 import serial
 import time
@@ -7,7 +6,7 @@ import time
 consumer_key = ''
 consumer_secret = ''
 access_token = ''
-access_token_secret = ''
+access_token_secret = '''
  
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -15,16 +14,18 @@ auth.set_access_token(access_token, access_token_secret)
  
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
+
+
 # Sample method, used to update a status
 #api.update_status(status='En fin... La noche es joven...')
-print ("Preparando b�squeda:")
+print ("Preparando búsqueda:")
 
 
 busqueda=tweepy.Cursor(api.search, q='sorteo RT',lang='es').items()
 #El string presentacion me permite avisar a mis followers que inicio al bot.
 presentacion='Atentos followers que inicio experimento con bot buscasorteos. Don\'t be afraid. Ni puto caso, vamos. Con fecha: '+time.strftime("%I:%M:%S")
 print (presentacion)
-api.update_status(status=presentacion)
+#api.update_status(status=presentacion)
 while True:
    
       
@@ -38,20 +39,55 @@ while True:
             
             if (not("b'RT" in str(frase))):
                   print ("Tweet original")
-                  tuit.retweet()
+                  api.retweet(tuit.id)
                   api.create_friendship(tuit.author.id)
                   time.sleep(10)
-                 
-      except TweepError:
+                  '''
+      except tweepy.TweepError:
             print ("Error de Tweepy")
-            time.sleep(10)
+            time.sleep(60*15)
             busqueda=tweepy.Cursor(api.search, q='#Arduino',lang='es').items()
             continue
-            
-     
+            '''
       except StopIteration:
             busqueda=tweepy.Cursor(api.search, q='#Arduino',lang='es').items()
             print ("Busqueda terminada. Descansando...")
             time.sleep(60)
             continue
 
+
+"""
+while True:
+      try:
+            for follower in tweepy.Cursor(api.followers, id='ieseduardovalen').items(150):
+                  api.send_direct_message(user_id=follower.id,text='¡Hola! Te mando DM para agradecerte que sigas al @ieseduardovalen (soy su CM) y para desearte un buen mes de Agosto. ¡Un saludo!')
+                  time.sleep(20)
+                  tuits=tuits+1
+                  if (tuits>20):
+                        tuits=0
+                        time.sleep(300)
+      except tweepy.TweepError:
+            print ('We got a timeout ... Sleeping for 15 minutes')
+            time.sleep(60*15)
+            print ('I am live')
+            user = next(users)
+            print (user.screen_name)
+      except StopIteration:
+            break
+
+while(1):
+      for tweets in tweepy.Cursor(api.search,q="#Arduino",lang="es").items():
+            print (tweets.text)
+            if (tweets.retweet_count>50):
+                  print ("Retuiteando...")
+                  tweets.retweet
+                  time.sleep(30)
+                  
+            tuits+=1
+            print(tuits)
+            if (tuits>100):
+                  tuits=0
+                  time.sleep(30)
+
+ 
+"""
